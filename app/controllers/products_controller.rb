d5de09c69742481434feee8de4820786d434e217
kathd <kathleensdomingo@gmail.com>
2017-08-31 19:14:42 -0700

    forms added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,18 @@ class ProductsController < ApplicationController
     render "all_products.html.erb"
   end
 
+  def form
+    render "form.html.erb"
+  end
+
+  def form_results
+    @new_product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image: params[:image],
+      description: params[:description])
+    @new_product.save
+    render "form_results.html.erb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get "/all_products" => "products#all_method"
+
+  get "/form" => "products#form"
+  post "/form_results" => "products#form_results"
 end
